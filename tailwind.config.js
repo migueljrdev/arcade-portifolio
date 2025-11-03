7618137c1dfb9a9b2b7a8b91237331b2a68a3d30
Miguel Jr <miguel.conta.jr@gmail.com>
2025-11-03 01:11:32 -0300

    add bg-body | header e nav estruturados no conteiner | div space animation | add arquivo script.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./index.html",
     "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        '100-4': '100% 4px',
+      },
+      backgroundImage: {
+        'gradient-custom': 'linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.06) 50%, rgba(255, 255, 255, 0) 100%)',
+      },
+      colors: {
+        'dark-primary': '#0a0a20',
+        'secondary-color': '#00ffff',
+        'yellow-color': '#ffff00',
+        'roxo-color': '#6a0dad',
+        'roxo-light-color': '#ff00ff',
+      },
+    },
   },
   plugins: [],
 }
